3f5568120ef0d167886bc526f05717d234e7dc26
KiRBi <karimkulovakerbez@gmail.com>
2024-03-31 17:40:03 +0600

    added Details screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:neobis_flutter_figma/presentation/screens/details_screen.dart';
+import 'package:neobis_flutter_figma/presentation/screens/home_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: DetailsScreen(),
+      home: HomeScreen(),
     );
   }
 }
